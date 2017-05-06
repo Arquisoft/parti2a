2ce5316fac0f9fb57321ab47077695f6d48ac476
RubenGarciaR <rubi_ruben_22@hotmail.com>
2017-05-06 12:01:38 +0200

    Mejorado formulario de actualizar información

--- src/test/java/uo/asw/selenium/ModificaPassword.java
+++ src/test/java/uo/asw/selenium/ModificaPassword.java
@@ -9,18 +9,18 @@ public class ModificaPassword {
 	
 	 public void rellenaFormulario(WebDriver driver, String anterior, String nueva) throws InterruptedException
 	   {
-			WebElement passanterior = driver.findElement(By.xpath("/html/body/form/table/tbody/tr[1]/td[2]/input"));
+			WebElement passanterior = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/form/table/tbody/tr[1]/td[2]/div/input"));
 			passanterior.click();
 			passanterior.clear();
 			passanterior.sendKeys(anterior);
 			
-			WebElement pasnueva = driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td[2]/input"));
+			WebElement pasnueva = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/form/table/tbody/tr[2]/td[2]/div/input"));
 			pasnueva.click();
 			pasnueva.clear();
 			pasnueva.sendKeys(nueva);
 			
 			//Pulsar el boton de email
-			By boton = By.xpath("/html/body/form/table/tbody/tr[3]/td[1]/input");
+			By boton = By.xpath("/html/body/div/div/div/div[2]/form/div/div[2]/input[1]");
 			driver.findElement(boton).click();	   
 			Thread.sleep(5000);
 	   }
